88931ab62c4be417430320cbdb245dcacc3e1c4c
Kevin Goyes <kevinfgoyesa@gmail.com>
2025-01-27 21:26:37 -0500

    added info about me

--- src/data/info.ts
+++ src/data/info.ts
@@ -9,45 +9,60 @@ export const info = {
   cv: "Cv_english_KevinGoyes.pdf",
 
   about: {
-    description: `I'm a full stack developer with a passion for causing a little chaos in web development.
-    I have a solid foundation in computer science, although I prefer skateboarding over studying.
-    I excel in building web applications using React, Node.js, and MongoDB.
-    I'm known for my sharp wit and quick thinking, traits that serve me well in coding and pranks alike.
-    Looking for a challenge? Let's create something memorable together.`,
+    description: `I’m Kevin Felipe Goyes Alfonso, a Marketing and International Business professional with a C1 English. I have experience as a Marketing Specialist, Marketing Leader, Digital Trafficker, and Community Manager. I’m proficient with tools like SEMRush, HubSpot, Zoho Projects, and technologies such as Python, JavaScript, React, and SQL, as well as platforms like Meta Ads and Google Ads. Currently, I’m specializing in programming focused on Artificial Intelligence and data analysis, developing skills to innovate and optimize digital strategies.`,
     education: [
       {
-        title: "Self-Taught Developer Extraordinaire",
-        date: "2000 - Present",
-        location: "Springfield Elementary School of Hard Knocks",
-        gpa: "4.0 (A+ in Pranks)",
-        thesis: "The Art of Subverting Expectations: A Practical Guide",
+        title: "Professional in Marketing and International Business",
+        date: "2018 - 2022",
+        location: "Sergio Arboleda University, Colombia",
+      },
+      {
+        title: "English Certification (B2 Level)",
+        date: "2023 - 2024",
+        location: "Elsis Institute, Australia",
+      },
+      {
+        title: "Programming Specialization (AI and Data Analysis)",
+        date: "2023 - Present",
+        location: "Platzi",
       },
     ],
     experience: [
       {
-        title: "Frontend Developer",
-        date: "2015 - Present",
-        location: "Krusty Burger Web Dev",
+        title: "Marketing Leader EanCampuslands",
+        date: "2024 - 2024",
+        location: "Ean University",
         description:
-          "Crafted interactive web experiences using React and Redux. Often multitasked between coding and avoiding Principal Skinner.",
+          "I specialize in the creation and implementation of innovative commercial strategies, leveraging both BTL and ATL approaches to achieve impactful results. My expertise includes managing the recruitment of new students and investors through targeted marketing initiatives. Additionally, I excel in data monitoring and analysis using tools like SQL and SAP, enabling data-driven decision-making to optimize performance.",
       },
       {
-        title: "Backend Bandit",
-        date: "2013 - 2015",
-        location: "Comic Book Guy's Software Emporium",
+        title: "Team Leader",
+        date: "2023 - 2023",
+        location: "The Plant Management Company",
         description:
-          "Masterminded backend solutions using Node.js and MongoDB. Surprisingly good at debugging, considering my history of causing bugs.",
+          "As a team leader, I excel in task delegation and goal achievement, ensuring efficient work inventory organization and seamless monitoring of daily objectives. I prioritize clear and direct communication with clients to maintain strong relationships and deliver exceptional results.",
+      },  
+      {
+        title: "Trafficker digital",
+        date: "2022 - 2023",
+        location: "Axioma b2b",
+        description:
+          "Creation of digital campaigns on Meta Ads, Google Ads, and LinkedIn Ads. Execution of processes such as webinars, web banners, and news on digital platforms. Monitoring and analysis of data on social media.",
       },
       {
-        title: "Junior Hacker",
-        date: "2010 - 2013",
-        location: "Springfield Elementary Cyber Club",
-        description:
-          "Pioneered early web exploits, leading the club in daring coding escapades. Was disciplined for hacking the school's grading system.",
+        title: "Analyst Jr (marketing, e-commerce)",
+        date: "2021 - 2022",
+        location: "Claro Company",
       },
+      {
+        title: "Community manager (marketing, e-commerce)",
+        date: "2020 - 2021",
+        location: "Texano Jeans",
+      },
+      
     ],
 
-    skills: ["JavaScript", "React", "Node.js", "MongoDB", "Prank Mastery"], // not used yet
+    skills: ["Digital campaign creation (Meta Ads, Google Ads, LinkedIn Ads)", "Marketing strategy development (BTL and ATL)", "Data analysis (SQL, Python)", "Social Media and Website Management", "Content strategy creation", "Community management", "Team leadership and task management", "E-commerce and marketing expertise", "Knowledge in programming: Python, JavaScript, React", "Data Science & Development", "UX/UI design for intuitive and visually appealing user experiences", "Advanced English proficiency", "HubSpot", "SEMrush", "Zoho Project", "Campaign Monitor"], // not used yet
   },
 
   projects: [
@@ -84,9 +99,8 @@ export const info = {
   ],
 
   contact: {
-    email: "bart@thesimpsons.com",
-    linkedin: "https://www.linkedin.com/in/bartsimpsonfake",
-    github: "https://www.github.com/gio-del",
-    twitter: "https://www.twitter.com/bartsimpsonfake",
+    email: "kevinfgoyes@gmail.com",
+    linkedin: "https://www.linkedin.com/in/kevin-felipe-goyes-alfonso-640570189?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+    github: "https://github.com/KevinGoyes",
   },
 };
